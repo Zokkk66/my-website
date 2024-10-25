193bcc6ca84c24a2b546561fbc966b4d09cd7fbd
佐罗斯 <136051963+Zokkk66@users.noreply.github.com>
2024-10-26 00:36:47 +0800

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// 获取音频元素和播放按钮元素
-var audio = document.getElementById('myAudio');
-var playButton = document.getElementById('playButton');
-
-// 为播放按钮添加点击事件监听器
-playButton.addEventListener('click', function () {
-    // 播放音频
-    audio.play();
-});
